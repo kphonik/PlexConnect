115b259f8a15228d924cf66a9a51906a998b8c5c
CyberGhost84 <yvesflammang@hotmail.com>
2015-02-17 21:24:40 +0100

    Revert "update latest commit"
    This reverts commit 57453ded73a179b0a56cd75eff4db8c82f18c5c0, reversing
    changes made to 4066fb1d29df2049990dd0bb3716b9eac58ee318.

--- PILBackgrounds.py
+++ PILBackgrounds.py
@@ -459,7 +459,10 @@ def generate(PMS_uuid, url, authtoken, resolution, blurRadius, gradientTemplate,
 
         background.paste(layer, ( 0, 0), layer)
 
-    
+    except:
+        dprint(__name__, 0, 'Error - Failed to modify image')
+        return "/thumbnails/Background_blank_" + resolution + ".jpg"
+
     background = textToImage(stylepath, background, resolution, titleText, titleSize, textColor, align, valign, offsetx, offsety)
 
     if subtitleText != "":
@@ -481,10 +484,11 @@ def generate(PMS_uuid, url, authtoken, resolution, blurRadius, gradientTemplate,
             background = textToImage(stylepath, background, resolution, statusText, statusSize, textColor, "right", "top", statusX, statusY)
 
 
+    try:
         # Save to Cache
         background.save(cachepath+"/"+cachefile)
     except:
-        dprint(__name__, 0, 'Error - Failed to generate background image.\n{0}', traceback.format_exc())
+        dprint(__name__, 0, 'Error - Failed to save image file')
         return "/thumbnails/Background_blank_" + resolution + ".jpg"
     
     dprint(__name__, 1, 'Cachefile  generated.')  # Debug
